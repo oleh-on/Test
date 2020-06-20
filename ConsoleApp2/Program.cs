38d6dc9bc0886334693538624845ff16ca06a585
Олег Онкалюк <olehonkalyuk@outlook.com>
2020-06-20 18:31:42 +0300

    Add new dll Person.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MyLib;
 
 namespace ConsoleApp2
 {
@@ -7,6 +8,9 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello from C++");
+
+            Person person = new Person( "Tom", 35 );
+            Console.WriteLine(person.Name + " " + person.Age);
         }
     }
 }
